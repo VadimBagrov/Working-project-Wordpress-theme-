a908707ac682d754f1bae16fe10ab26ec78b9c96
Vadim Bagrov <110101692+VadimBagrov@users.noreply.github.com>
2022-11-07 15:41:30 +0300

    Ready Header and Footer + Menu Button
    Ready Header and Footer(Html+Scss) + Menu Button(JS,Html,Scss)

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.header__btn').on('click' , function () {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+
+    $('.rightside-menu__close').on('click' , function () {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+
+}); //Появляющиеся сбоку меню on/off
