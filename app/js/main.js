493f0f08ad9e93f4cc23f2268cc29c2dd5f61c88
Vadim Bagrov <110101692+VadimBagrov@users.noreply.github.com>
2022-11-08 09:49:45 +0300

    Slider ready (using the library)
    Plugged in the slick-carousel library and configured the main slider

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,10 @@ $(function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
 
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true, //Чтобы менялась плавно\
+        autoplay: true //Автоматически меняется слайд(стандарт 3 сек)
+    }) //Активация слайдера(slick-carousel)
 }); //Появляющиеся сбоку меню on/off
