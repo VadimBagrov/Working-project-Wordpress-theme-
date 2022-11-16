f29381f583096c2874a9dd7c882c7b5ec4b0f561
Vadim Bagrov <110101692+VadimBagrov@users.noreply.github.com>
2022-11-16 22:38:41 +0300

    The layout is complete 16.11.2022
    The layout is complete
    
    Minor edits and adaptive design remain, the finished version will be minimized

--- app/js/main.js
+++ app/js/main.js
@@ -16,12 +16,22 @@ $(function () {
     }) //Активация слайдера(slick-carousel)
 }); //Появляющиеся сбоку меню on/off
 
+// Слайдер 3 точки внизу 10 картинок вверху
 $('.contact-slider').slick({
     slidesToShow: 10, //изначально показывается 10 штук
     slidesToScroll: 10, //перемещать сразу по 10
     dots: true,
     arrows: false,
 })
+
+
+// Слайдер стрелки по бокам
+$('.article-slider__box').slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow-slide-left"></button>' ,
+
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="arrow-slide-right"></button>'
+});
+
 
 
 var mixer = mixitup('.gallery__inner' , {
